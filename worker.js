4b2134725e90514183ae5bc06bae215540dc873a
ZMzakay <zmzakay@gmail.com>
2026-09-15 19:23:12 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,7 +2,8 @@ import {
     WebWorkerMLCEngineHandler
 } from "https://esm.run/@mlc-ai/web-llm";
 
-const handler = new WebWorkerMLCEngineHandler();
+const handler =
+    new WebWorkerMLCEngineHandler();
 
 self.onmessage = (message) => {
     handler.onmessage(message);
